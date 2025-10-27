be5797d68d34bb5a4e7a6773030b74d4194bf74c
facu_v <facundoandreassi@gmail.com>
2025-10-27 08:59:39 -0300

    Schemas y Models

---
+++ server/models/Category.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+// Category Schema 
+const categorySchema = new mongoose.Schema({ 
+name: { 
+type: String, 
+required: true, 
+unique: true, 
+trim: true 
+}, 
+description: String, 
+parentCategory: { 
+type: mongoose.Schema.Types.ObjectId, 
+ref: 'Category', 
+default: null 
+  }, 
+  isActive: { 
+    type: Boolean, 
+    default: true 
+  } 
+});
+
+export const Category = mongoose.model("Category", categorySchema)
