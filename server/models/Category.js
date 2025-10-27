2cb2c271373e701d791ab0df03f2bfa70f705f35
facu_v <facundoandreassi@gmail.com>
2025-10-27 09:03:09 -0300

    Update Category.js

--- server/models/Category.js
+++ server/models/Category.js
@@ -2,21 +2,21 @@ import mongoose from "mongoose";
 
 // Category Schema 
 const categorySchema = new mongoose.Schema({ 
-name: { 
-type: String, 
-required: true, 
-unique: true, 
-trim: true 
-}, 
-description: String, 
-parentCategory: { 
-type: mongoose.Schema.Types.ObjectId, 
-ref: 'Category', 
-default: null 
-  }, 
-  isActive: { 
-    type: Boolean, 
-    default: true 
+    name: { 
+        type: String, 
+        required: true, 
+        unique: true, 
+        trim: true 
+    }, 
+    description: String, 
+    parentCategory: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Category', 
+        default: null 
+    }, 
+    isActive: { 
+        type: Boolean, 
+        default: true 
   } 
 });
 
